95ef5d8be824fff7ef01f7bb8f4d8d591b62cf7b
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2026-01-19 21:22:27 +0530

    deleted src folder in backend and api route working

--- apps/backend/services/userService.ts
+++ apps/backend/services/userService.ts
@@ -3,6 +3,9 @@ import { SignupData } from "../validators/userSchema.js";
 
 export const signupService =async(data:SignupData)=>{
    try {
+
+    console.log("yha aa gye")
+    console.log(data,'dekho data')
     const {email,password,binanceApiKey,binanceSecretKey}=data
       const newUser=await prisma.user.create({
         data:{
@@ -12,6 +15,7 @@ export const signupService =async(data:SignupData)=>{
           binanceSecretKey
         }
       })
+      console.log(newUser,"dekho new user")
       return newUser
    }catch(error){
      throw error
