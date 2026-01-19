e996442087ce3fbfef230fb66b68b7d1554e5402
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2026-01-19 12:52:00 +0530

    all services running

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,17 +1,12 @@
-import dotenv from "dotenv";
+
 
 import { prisma } from "@repo/db";
-dotenv.config();
+
 
 const main = async () => {
-  console.log("DB:", process.env.DATABASE_URL);
 
-  const users = await prisma.user.create({
-    data: {
-      email: "piyush02040@gmail.com",
-      name: "Piyush Kumar Singh",
-    },
-  });
+
+  const users = await prisma.user.findMany();
   console.log(users);
 
 }
