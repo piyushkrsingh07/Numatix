ab65a415e603e1cffdd747d072ebe3fa5684ed78
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2026-01-19 15:51:55 +0530

    started server configuration

---
+++ apps/backend/router/user.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const app:Router = Router()
+
+export default app
--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,13 +1,33 @@
+import dotenv from "dotenv"
+
+import express,{Request,Response} from 'express'
+import cookieparser from 'cookie-parser'
+import cors from "cors"
+// import AuthRoutes from '../router/user.js'
+dotenv.config();
+
+const app = express()
+app.use(cors())
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(cookieparser())
+
+app.get('/',(req:Request,res:Response):Response=>{
+    return res.send("Server is Live!")
+})
+
+// app.use('/auth',AuthRoutes)
+
+const port:number = Number(process.env.PORT) || 5000
+
+app.listen(port,()=>{
+    console.log(`server is running at http://localhost:${port}`)
+})
 
 
-import { prisma } from "@repo/db";
 
 
-const main = async () => {
 
 
-  const users = await prisma.user.findMany();
-  console.log(users);
 
-}
-main();
