1b6e722453f45582a9284cec182bedddfa144f5d
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2026-01-24 17:00:19 +0530

    socket initialized from backend

---
+++ apps/event-service/server.ts
@@ -0,0 +1,65 @@
+import dotenv from "dotenv"
+import {redisConfig} from "@repo/db"
+import http from 'http'
+
+import express,{Request,Response} from 'express'
+import cookieparser from 'cookie-parser'
+import cors from "cors"
+import { channel } from "diagnostics_channel"
+import { emitOrderToSocket, initializeSocket } from "./services/socket"
+
+
+dotenv.config();
+
+const app = express()
+app.use(cors())
+
+app.use(express.urlencoded({extended:false}))
+app.use(express.json())
+app.use(cookieparser())
+
+app.get('/',(req:Request,res:Response):Response=>{
+    return res.send("event Execution is Live!")
+})
+
+const port:number = Number(process.env.PORT) || 4010
+
+
+const server=http.createServer(app)
+
+    initializeSocket(server)
+async function start(){
+    try{
+ const redisSub=await redisConfig.sub.subscribe('events:order:status')
+ console.log(redisSub,'dekho redis sub')
+
+ redisConfig.sub.on('message',async(channel:string,message:string)=>{
+    if(channel !== "events:order:status") return
+    console.log(JSON.parse(message).Message,'dekho message finally')
+      const data:Record<string,any>=JSON.parse(message).Message
+    emitOrderToSocket(data.userId,{
+        type:"ORDER_UPDATE",
+        data:{
+            orderId:data.userId,
+            status:data.status,
+            symbol:data.symbol,
+            price:data.price
+        }
+    })
+
+
+ })
+
+    }catch(error){
+      console.log(error)
+      
+    }
+}
+
+server.listen(port,()=>{
+    console.log(`server hello is running at http://localhost:${port}`)
+})
+
+start().catch((e)=>{
+    console.log("execution service crashed")
+})
---
+++ apps/event-service/services/socket.ts
@@ -0,0 +1,47 @@
+import { Server as HttpServer } from "http"
+import { Server as SocketIOServer,Socket } from "socket.io";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+
+
+let io:SocketIOServer
+
+
+export  const initializeSocket = (server: HttpServer) => {
+   io = new SocketIOServer(server, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  io.use((socket,next)=>{
+    try{
+    const token=socket.handshake.auth.token
+    if(!token){
+        return next(new Error('No token provided'))
+    }
+   const jwtresponse= jwt.verify(token,process.env.JWT_SECRET as string) as JwtPayload
+   console.log(jwtresponse,'see jwt response')
+
+   socket.data.userId=jwtresponse.userId
+    }catch(error){
+        return next(new Error("Invalid token provided"))
+    }
+
+
+  })
+ 
+  io.on('connection',(socket:Socket)=>{
+    console.log('new socket connected',socket.id)
+     const userId=socket.data.userId
+      socket.join(userId)
+
+  })
+
+
+}
+
+export const emitOrderToSocket=(userId:string,data:Record<string,any>)=>{
+     io.to(userId).emit("ORDER_UPDATE",data)
+}
+
+
