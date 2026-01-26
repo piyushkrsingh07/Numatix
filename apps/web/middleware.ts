85a88bc18b65bf09839b30151f7ad2e639f5a0ab
PIYUSH KUMAR SINGH <piyush02040@gmail.com>
2026-01-27 02:57:02 +0530

    removed middleware for now

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -1,28 +1,28 @@
-// import {  NextResponse } from "next/server";
-// import type { NextRequest } from "next/server";
+import {  NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 
-// const protectedRoute=['/trading']
+const protectedRoute=['']
 
 
 
-// export function middleware(request:NextRequest){
+export function middleware(request:NextRequest){
 
-//     const token = request.cookies.get('token')?.value
-//     console.log('see token in middlkeware',token)
-//         const isProtected=protectedRoute.some((route)=>request.nextUrl.pathname.startsWith(route))
-//   if( !token && isProtected){
-// return NextResponse.redirect(new URL('/auth/signIn', request.url))
-//   }
+    const token = request.cookies.get('token')?.value
+    console.log('see token in middlkeware',token)
+        const isProtected=protectedRoute.some((route)=>request.nextUrl.pathname.startsWith(route))
+  if( !token && isProtected){
+return NextResponse.redirect(new URL('/auth/signIn', request.url))
+  }
 
-//   return NextResponse.next();
-// };
+  return NextResponse.next();
+};
 
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// };
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+};
